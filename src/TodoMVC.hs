1e73a2ac571459c9307b31810a936b9ef6227e1a
Dave Laing <dave.laing.80@gmail.com>
2017-11-21 09:03:23 +1000

    Adds a bit more to the TodoMVC work.

--- src/TodoMVC.hs
+++ src/TodoMVC.hs
@@ -13,8 +13,10 @@ module TodoMVC (
     todomvc
   ) where
 
-import Control.Lens
+import Control.Monad (void)
 import Data.Monoid ((<>))
+
+import Control.Lens
 
 import Data.Text (Text)
 import qualified Data.Text as Text
@@ -52,6 +54,14 @@ buttonDynAttr ::
 buttonDynAttr dAttrs label = do
   (e, _) <- elDynAttr' "button" dAttrs $ text label
   pure $ domEvent Click e
+
+data TodoItem =
+  TodoItem {
+    _tiComplete :: Bool
+  , _tiText :: Text
+  } deriving (Eq, Ord, Show)
+
+makeLenses ''TodoItem
 
 data HeaderState (v :: * -> *) =
   HeaderState
@@ -103,31 +113,151 @@ data MainState (v :: * -> *) =
   MainState
   deriving (Eq, Ord, Show)
 
-mainSection ::
+complete ::
   MonadWidget t m =>
   Bool ->
+  Event t Bool ->
+  Event t () ->
+  m (Event t Bool, Event t ())
+complete initial eMarkAllComplete eClearComplete = do
+  cb <- checkbox initial $ def
+    & checkboxConfig_setValue .~ eMarkAllComplete
+    & checkboxConfig_attributes .~ pure ("class" =: "toggle")
+
+  let
+    eChange = cb ^. checkbox_change
+    eRemove = gate (current $ cb ^. checkbox_value) eClearComplete
+
+  pure (eChange, eRemove)
+
+textRead ::
+  MonadWidget t m =>
+  Dynamic t Text ->
+  m (Event t ())
+textRead dText = do
+  (e, _) <- el' "label" . dynText $ dText
+  pure . void $ domEvent Dblclick e
+
+remove ::
+  MonadWidget t m =>
+  m (Event t ())
+remove =
+  buttonDynAttr (pure $ "class" =: "destroy") ""
+
+todoItemRead ::
+  MonadWidget t m =>
+  Event t Bool ->
+  Event t () ->
+  Dynamic t TodoItem ->
+  m (Event t (TodoItem -> TodoItem), Event t (), Event t())
+todoItemRead eMarkAllComplete eClearComplete dItem = elClass "div" "view" $ do
+  (eComplete, eRemoveComplete) <- complete False eMarkAllComplete eClearComplete
+
+  eEditStart <- textRead $ view tiText <$> dItem
+  eRemoveClick <- remove
+
+  let
+    eChange = set tiComplete <$> eComplete
+    eRemove = leftmost [eRemoveClick, eRemoveComplete]
+
+  pure (eChange, eRemove, eEditStart)
+
+todoItemWrite ::
+  MonadWidget t m =>
+  Dynamic t TodoItem ->
+  m (Event t (TodoItem -> TodoItem), Event t (), Event t())
+todoItemWrite dItem = do
+  ti <- textInput $ def
+    & textInputConfig_attributes .~ pure ("class" =: "edit")
+
+  let
+    eChange = set tiText <$> never
+    eRemove = never
+    eEditStop = never
+
+  pure (eChange, eRemove, eEditStop)
+
+todoItem ::
+  MonadWidget t m =>
+  Event t Bool ->
+  Event t () ->
+  Dynamic t TodoItem ->
+  m (Event t (TodoItem -> TodoItem), Event t ())
+todoItem eMarkAllComplete eClearComplete dItem = mdo
+  let
+    mkCompleted False = ""
+    mkCompleted True = "completed "
+    dCompleted = mkCompleted . view tiComplete <$> dItem
+    mkEditing False = ""
+    mkEditing True = "editing "
+    dEditing = mkEditing <$> dEdit
+
+  dEdit <- holdDyn False eEdit
+  (eChange, eRemove, eEdit) <- elDynClass "li" (dCompleted <> dEditing) $ mdo
+
+    (eChangeRead, eRemoveRead, eEditStart) <- todoItemRead eMarkAllComplete eClearComplete dItem
+    (eChangeWrite, eRemoveWrite, eEditStop) <- todoItemWrite dItem
+
+    let
+      eChange = mergeWith (.) [eChangeRead, eChangeWrite]
+      eRemove = leftmost [eRemoveRead, eRemoveWrite]
+      eEdit = leftmost $ [True <$ eEditStart, False <$ eEditStop]
+
+    pure (eChange, eRemove, eEdit)
+
+  pure (eChange, eRemove)
+
+todoList ::
+  MonadWidget t m =>
+  Map Int TodoItem ->
+  Event t Text ->
+  Event t Bool ->
+  Event t () ->
+  m (Dynamic t (Map Int TodoItem))
+todoList initial eAdd eMarkAllComplete eClearComplete = mdo
+  dCount <- count eAdd
+  let
+    initialCount = (+ 1) . maximum . (0 :) . Map.keys $ initial
+    dKey = (+ initialCount) <$> dCount
+    eItem = TodoItem False <$> eAdd
+
+  dMap <- foldDyn ($) initial . mergeWith (.) $ [
+            Map.insert <$> current dKey <@> eItem
+          , Map.mergeWithKey (\_ f x -> Just (f x)) (const mempty) id <$> eChanges
+          , flip (foldr Map.delete) <$> eRemoves
+          ]
+
+  dOut <- elClass "ul" "todo-list" . list dMap $
+    todoItem eMarkAllComplete eClearComplete
+
+  let
+    eChanges = switch . current . fmap (mergeMap . fmap fst) $ dOut
+    eRemoves = fmap Map.keys . switch . current . fmap (mergeMap . fmap snd) $ dOut
+
+  pure dMap
+
+mainSection ::
+  MonadWidget t m =>
+  Map Int TodoItem ->
+  Event t Text ->
   Event t () ->
   m (Dynamic t Int, Dynamic t Bool)
-mainSection initialAllComplete eClearComplete = mdo
+mainSection initial eAdd eClearComplete = mdo
   let
     mkClass 0 = "hidden "
     mkClass _ = ""
     sClass = "main "
     dClass = pure sClass <> mkClass <$> dSize
+    initialAllComplete = all (view tiComplete) initial
   (dSize, dAny) <- elDynClass "section" dClass $ mdo
       eMarkAllComplete <- markAllComplete initialAllComplete dAll
 
-      dMap <- foldDyn ($) Map.empty . mergeWith (.) $ [
-              ]
-
-      dOut <- elClass "ul" "todo-list" . listWithKey dMap $ \k dv -> do
-        pure ()
-
+      dMap <- todoList initial eAdd eMarkAllComplete eClearComplete
 
       let
-        dSize = pure 2
-        dAny = pure True
-        dAll = pure False
+        dSize = Map.size <$> dMap
+        dAny = any (view tiComplete) <$> dMap
+        dAll = all (view tiComplete) <$> dMap
       pure (dSize, dAny)
   pure (dSize, dAny)
 
@@ -182,11 +312,22 @@ footer initialAnyComplete dSize dAny =
       todoCount dSize
       clearComplete initialAnyComplete dAny
 
-todomvc :: JSM ()
-todomvc =
-  mainWidgetWithHead headSection $
-    elClass "section" "todoapp" $ mdo
-      eAdd <- header
-      (dSize, dAny) <- mainSection False eClearComplete
-      eClearComplete <- footer False dSize dAny
-      pure ()
+todomvc :: Map Int TodoItem -> JSM ()
+todomvc initial =
+  let
+    initialAnyComplete = any (view tiComplete) initial
+  in
+    mainWidgetWithHead headSection $
+      elClass "section" "todoapp" $ mdo
+        eAdd <- header
+        (dSize, dAny) <- mainSection initial eAdd eClearComplete
+        eClearComplete <- footer initialAnyComplete dSize dAny
+        pure ()
+
+todomvcExample :: JSM ()
+todomvcExample =
+  todomvc . Map.fromList $
+    [ (1, TodoItem False "A")
+    , (2, TodoItem True "B")
+    , (3, TodoItem False "C")
+    ]
