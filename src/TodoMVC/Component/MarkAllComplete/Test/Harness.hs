d90faee9c84e4ef1afdd3c532bf5516d83e9df7e
Dave Laing <dave.laing.80@gmail.com>
2017-11-24 17:02:01 +1000

    Adds a harness for MarkAllComplete

--- src/TodoMVC/Component/MarkAllComplete/Test/Harness.hs
+++ src/TodoMVC/Component/MarkAllComplete/Test/Harness.hs
@@ -5,5 +5,27 @@ Maintainer  : dave.laing.80@gmail.com
 Stability   : experimental
 Portability : non-portable
 -}
+{-# LANGUAGE OverloadedStrings #-}
 module TodoMVC.Component.MarkAllComplete.Test.Harness (
   ) where
+
+import Reflex.Dom.Core
+
+import Reflex.Test
+
+import TodoMVC.Common
+import TodoMVC.Component.MarkAllComplete
+import TodoMVC.Component.MarkAllComplete.Test
+
+markAllCompleteHarness ::
+  MonadWidget t m =>
+  m ()
+markAllCompleteHarness = do
+  eClick <- buttonDynAttr (pure $ "id" =: "test-toggle") "Toggle"
+  dToggle <- toggle True eClick
+  eComplete <- markAllComplete False dToggle
+  dComplete <- holdDyn False eComplete
+  elAttr "div" ("id" =: "test-complete") $
+    display dComplete
+
+  pure ()
